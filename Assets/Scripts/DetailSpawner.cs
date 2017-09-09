77d136cfdb9c9bcb58b6ce9d20aa128021ca3f17
jacobnelson <jacob.nelson.dev@gmail.com>
2017-09-09 14:16:01 -0400

    Added acorns, adjusted DetailSpawner a bit, Health, Score, and Game Over, sound effects

--- Assets/Scripts/DetailSpawner.cs
+++ Assets/Scripts/DetailSpawner.cs
@@ -8,6 +8,7 @@ public class DetailSpawner : MonoBehaviour {
     public GameObject prefabBranch3;
     public GameObject prefabLichen;
     public GameObject prefabMoss;
+    public GameObject prefabAcorn;
 
 
     // Use this for initialization
@@ -15,11 +16,16 @@ public class DetailSpawner : MonoBehaviour {
     {
 
         PlaceOnSurface(prefabBranch3);
-        PlaceOnSurface(prefabLichen);
-        PlaceOnSurface(prefabMoss);
+        PlaceOnSurface(prefabBranch3);
+        PlaceOnSurface(prefabBranch3);
+        PlaceOnSurface(prefabLichen, .22f);
+        PlaceOnSurface(prefabLichen, .22f);
+        PlaceOnSurface(prefabMoss, .22f);
+        PlaceOnSurface(prefabMoss, .22f);
+        PlaceOnSurface(prefabAcorn, .5f);
 
     }
-    private void PlaceOnSurface(GameObject prefab)
+    private void PlaceOnSurface(GameObject prefab, float upAdjustment = 0)
     {
         Branch branch = GetComponent<Branch>();
         int n = (int)(Random.value * branch.finalPoints.Length);
@@ -28,7 +34,7 @@ public class DetailSpawner : MonoBehaviour {
         BranchMesh.BranchSurfaceProperties props =
             branch.mesh.GetSurfaceProperties(n, 0, l, 0);
 
-        Instantiate(prefab, props.position, Quaternion.LookRotation(props.forward, props.up), transform);
+        Instantiate(prefab, props.position + props.up * upAdjustment, Quaternion.LookRotation(props.forward, props.up), transform);
 
     }
 }
--- Assets/Scripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerStatus.cs
@@ -2,18 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerStatus : MonoBehaviour {
 
     public int currentHP = 16;
     public int maxHP = 16;
 
+    public int score = 0;
+
     public Text hpText;
+    public Text scoreText;
+
+    public AudioSource audioSource;
+    public AudioClip eatAcornSound;
+    public AudioClip[] squirrelYells;
 
 	// Use this for initialization
 	void Start () {
-        hpText.text = "HP: " + currentHP;
-	}
+        UpdateHPDisplay();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -24,17 +32,58 @@ public class PlayerStatus : MonoBehaviour {
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
         {
-            TakeDamage(1);
+            TakeDamage(4);
+        }
+        else if (other.gameObject.layer == LayerMask.NameToLayer("Acorn"))
+        {
+            Destroy(other.gameObject);
+            ReplenishHealth(1);
+            score += 100;
+            UpdateScoreDisplay();
+            PlaySound(eatAcornSound);
         }
     }
 
     public void TakeDamage(int amount)
     {
+        int index = Random.Range(0, 3);
+        PlaySound(squirrelYells[index]);
         currentHP -= amount;
         if (currentHP <= 0)
         {
             currentHP = 0;
+            Death();
         }
+        UpdateHPDisplay();
+    }
+
+    public void ReplenishHealth(int amount)
+    {
+        currentHP += amount;
+        if (currentHP >= maxHP)
+        {
+            currentHP = maxHP;
+        }
+        UpdateHPDisplay();
+    }
+
+    public void UpdateHPDisplay()
+    {
         hpText.text = "HP: " + currentHP;
+    }
+
+    public void UpdateScoreDisplay()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void Death()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 }
